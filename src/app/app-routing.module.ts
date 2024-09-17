07270ce14d7a349a7616262b83370cef8a6f29a7
HoyozaniChabalala <Chabalala@thedigitalacademy.co.za>
2024-09-17 22:26:55 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/app', pathMatch: 'full' },
+  { path:'app', component: HomePageComponent},
+  { path:'about-page', component: AboutPageComponent},
+  { path:'contact', component: ContactPageComponent},
+  { path:'portfolio', component: PortfolioPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    PortfolioPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
